e4d6ee73cf21628636874226070fc71f22ff9f85
ByronArgos <byronpaul88@gmail.com>
2015-07-09 11:57:33 -0500

    main(1)

--- proylpequipo2.cpp
+++ proylpequipo2.cpp
@@ -3,7 +3,20 @@
 #include <stdio>
 
 main()
-{
- //no sale
+{ int opc;
+do{
+cout<<".....---- menu----....."<<endl<<endl;
+cout<<"1.-Ingresar una nueva hora de clases"<<endl<<endl;
+cout<<"2.-Ver el horario de hoy" <<endl<<endl;
+cout<<"3.-Ver el horario de la semana"<<endl<<endl;
+cout<<"0.-Salir"<<endl<<endl;
+cin>>opc;
+clrscr();
+switch(opc)
+     {
+
+     }
+ }while (opc!=0);
+ getch();
 }
 
